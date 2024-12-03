d8570731762713366f9f9a144734a2a3546dc945
VanThanh2004 <levanthanh20045@gmail.com>
2024-12-03 10:07:25 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header"; // Phần Header giữ nguyên như cũ
+
+// Các trang của bạn
+import HomePage from "./pages/HomePage"; // Trang chủ
+import ShopPage from "./pages/ShopPage"; // Trang cửa hàng
+import MilkPage from "./pages/MilkPage"; // Trang sữa cho bé
+import ClothesPage from "./pages/ClothesPage"; // Trang quần áo
+import ToysPage from "./pages/ToysPage"; // Trang đồ chơi
+import CartPage from "./pages/CartPage"; // Trang giỏ hàng
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />{" "}
+      {/* Header giữ nguyên như cũ, hiển thị trên tất cả các trang */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />{" "}
+        {/* Trang chủ cũ vẫn làm trang chính */}
+        <Route path="/shop" element={<ShopPage />} /> {/* Trang cửa hàng */}
+        <Route path="/milk" element={<MilkPage />} /> {/* Trang sữa cho bé */}
+        <Route path="/clothes" element={<ClothesPage />} />{" "}
+        {/* Trang quần áo */}
+        <Route path="/toys" element={<ToysPage />} /> {/* Trang đồ chơi */}
+        <Route path="/cart" element={<CartPage />} /> {/* Trang giỏ hàng */}
+      </Routes>
+    </Router>
   );
 }
 
